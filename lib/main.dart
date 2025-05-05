aaa21f8007ea17f127b772e1f5d673290e918f0f
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-05-05 21:13:36 +0700

    Menambahkan Container

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,45 @@ class MyApp extends StatelessWidget {
                   letterSpacing: 2.5,
                   fontWeight: FontWeight.bold
               )
-              )
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.phone, color: Colors.teal,),
+                    SizedBox(width: 10.0,),
+                    Text("+62 812 3456 789", style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: "Source Sans 3",
+                      fontSize: 20.0),
+                    )
+                  ]
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 25.0
+                ),
+                child: Row(
+                    children: <Widget>[
+                      Icon(Icons.email, color: Colors.teal,),
+                      SizedBox(width: 10.0,),
+                      Text("kholis@gmail.com", style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: "Source Sans 3",
+                          fontSize: 20.0),
+                      )
+                    ]
+                ),
+              ),
             ],
           ),
         ),
